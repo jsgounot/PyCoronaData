8a4d7d509b9466a9d5ed690613b18b23e23afbe6
jsgounot <jsgounot@gmail.com>
2020-04-22 17:45:45 +0200

    Change version number

--- pycoronadata/__init__.py
+++ pycoronadata/__init__.py
@@ -2,8 +2,8 @@
 # @Author: jsgounot
 # @Date:   2020-03-25 19:21:42
 # @Last modified by:   jsgounot
-# @Last Modified time: 2020-04-17 13:34:40
+# @Last Modified time: 2020-04-22 17:45:35
 
 from pycoronadata.core import *
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
